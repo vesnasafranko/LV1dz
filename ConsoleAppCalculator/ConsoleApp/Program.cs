526d7ad592bc3866f26ebb6438538c78ed189b77
Korisnik <Korisnik@LAPTOP-LGT1VSQJ>
2017-10-31 12:08:50 +0100

    Vesnica

---
+++ ConsoleAppCalculator/ConsoleApp/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Calculation;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float nPrviBroj;
+            float nDrugiBroj;
+            float fRezultat = 0;
+            int nOdabir;
+
+            Console.WriteLine("Unesite prvi broj");
+            nPrviBroj = Convert.ToSingle(Console.ReadLine());
+
+            Console.WriteLine("Unesite drugi broj");
+            nDrugiBroj = Convert.ToSingle(Console.ReadLine());
+
+            Console.WriteLine("Odaberite opciju:");
+            Console.WriteLine("1.zbrajanje");
+            Console.WriteLine("2.mnozenje");
+            Console.WriteLine("3.oduzimanje");
+            Console.WriteLine("4.dijeljenje");
+
+            nOdabir = Convert.ToInt16(Console.ReadLine());
+            Calculator Calculator = new Calculator();
+            switch(nOdabir)
+            {
+                case 1:
+                    //fRezultat = nPrviBroj + nDrugiBroj;
+                    Calculator.Add(nPrviBroj, nDrugiBroj);
+                    break;
+                case 2:
+                    //fRezultat = nPrviBroj * nDrugiBroj;
+                    Calculator.Multiply(nPrviBroj, nDrugiBroj);
+                    break;
+                case 3:
+                    //fRezultat = nPrviBroj - nDrugiBroj;
+                    Calculator.Substract(nPrviBroj, nDrugiBroj);
+                    break;
+                case 4:
+                    //fRezultat = nPrviBroj / nDrugiBroj;
+                    Calculator.Divide(nPrviBroj, nDrugiBroj);
+                    break;
+            }
+            Console.WriteLine("Rezultat odabrane operacije je " + fRezultat);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ConsoleTimer/WindowsLoggingService/Service1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace WindowsLoggingService
+{
+    public partial class Service1 : ServiceBase
+    {
+        string sDateTime;
+        string sText;
+        public Service1()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            sDateTime = DateTime.Now.ToString("dd/MM/yyyy hh:mm:ss");
+            sText = "Servis je pokrenut " + sDateTime;
+            WriteToFile(sText);
+        }
+
+        protected override void OnStop()
+        {
+            sDateTime = DateTime.Now.ToString("dd/MM/yyyy hh:mm:ss");
+            sText = "Servis je zaustavljen " + sDateTime;
+            WriteToFile(sText);
+        }
+        private static void WriteToFile(string text)
+        {
+            string path = "C:\\Users\\Korisnik\\Documents\\VesnaS\\WindowsServiceVesnica.txt";
+            using (StreamWriter writer = new StreamWriter(path, true))
+            {
+                writer.WriteLine(string.Format(text, DateTime.Now.ToString("dd/MM/yyyy hh:mm:ss tt")));
+
+                writer.Close();
+            }
+        }
+    }
+}
